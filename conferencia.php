e72f19a6cb62d725374855c0a789d319f40b6330
GermanDenaro <germandenaroc@gmail.com>
2020-08-10 20:49:31 -0300

    php

---
+++ conferencia.php
@@ -0,0 +1,52 @@
+<?php include_once 'includes/templates/header.php'; ?>
+
+    <section class="seccion contenedor">
+        <h2>La mejor conferencia de diseño web en español</h2>
+        <p>
+            Quisque massa justo, auctor vel mi in, pellentesque sollicitudin diam. Integer consectetur nisi neque, eget malesuada risus finibus sed. Morbi sapien metus, facilisis in rutrum in, tristique eget est. Praesent ultricies nisl et venenatis molestie. Phasellus
+            ornare fringilla nisl ac varius. Ut sodales laoreet egestas. Aliquam dictum augue eget orci laoreet, molestie suscipit massa placerat. Curabitur at efficitur lacus, vel fringilla diam.
+        </p>
+
+    </section>
+    <!--seccion-->
+
+
+
+    <section class="seccion contenedor">
+        <h2>Galeria de Fotos</h2>
+
+        <div class="galeria">
+            <a href="img/galeria/01.jpg" data-lightbox="galeria">
+                <img src="img/galeria/thumbs/01.jpg">
+            </a>
+            <a href="img/galeria/2.jpg" data-lightbox="galeria">
+                <img src="img/galeria/thumbs/02.jpg">
+            </a>
+            <a href="img/galeria/03.jpg" data-lightbox="galeria">
+                <img src="img/galeria/thumbs/03.jpg">
+            </a>
+            <a href="img/galeria/04.jpg" data-lightbox="galeria">
+                <img src="img/galeria/thumbs/04.jpg">
+            </a>
+            <a href="img/galeria/05.jpg" data-lightbox="galeria">
+                <img src="img/galeria/thumbs/05.jpg">
+            </a>
+            <a href="img/galeria/06.jpg" data-lightbox="galeria">
+                <img src="img/galeria/thumbs/06.jpg">
+            </a>
+            <a href="img/galeria/07.jpg" data-lightbox="galeria">
+                <img src="img/galeria/thumbs/07.jpg">
+            </a>
+            <a href="img/galeria/08.jpg" data-lightbox="galeria">
+                <img src="img/galeria/thumbs/08.jpg">
+            </a>
+            <a href="img/galeria/09.jpg" data-lightbox="galeria">
+                <img src="img/galeria/thumbs/09.jpg">
+            </a>
+            <a href="img/galeria/10.jpg" data-lightbox="galeria">
+                <img src="img/galeria/thumbs/10.jpg">
+            </a>
+        </div>
+    </section>
+
+    <?php include_once 'includes/templates/footer.php'; ?>
---
+++ includes/templates/footer.php
@@ -0,0 +1,58 @@
+<footer class="site-footer">
+        <div class="contenedor clearfix">
+            <div class="footer-informacion">
+                <h3>Sobre <span>gdlwebcamp</span></h3>
+                <p>Donec dapibus tincidunt justo, vitae varius ex pretium at. Phasellus posuere odio nisl, mollis facilisis augue ultricies ac. Morbi eget odio eu ligula fringilla bibendum aliquam molestie arcu.</p>
+            </div>
+            <div class="ultimos-tweets">
+                <h3>Últimos <span>tweets</span></h3>
+                <ul>
+                    <li>Donec nec magna consectetur, suscipit sem ut, lobortis tortor. Mauris viverra auctor leo nec condimentum.. Vivamus posuere dui eu condimentum placerat.</li>
+                    <li>Donec nec magna consectetur, suscipit sem ut, lobortis tortor. Mauris viverra auctor leo nec condimentum.. Vivamus posuere dui eu condimentum placerat.</li>
+                    <li>Donec nec magna consectetur, suscipit sem ut, lobortis tortor. Mauris viverra auctor leo nec condimentum.. Vivamus posuere dui eu condimentum placerat.</li>
+                </ul>
+            </div>
+            <div class="menu">
+                <h3>Redes <span>sociales</span></h3>
+                <nav class="redes-sociales">
+                    <a href="#"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                    <a href="#"><i class="fab fa-pinterest-p"></i></a>
+                    <a href="#"><i class="fab fa-youtube"></i></a>
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                </nav>
+            </div>
+        </div>
+
+        <p class="copyright">Todos los derechos Reservados gdlwebcamp 2020 ©.</p>
+
+    </footer>
+
+    <script src="js/jquery-1.12.0.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="js/main.js "></script>
+    <script src="js/vendor/modernizr-3.11.2.min.js "></script>
+    <script src="js/jquery.lettering.js "></script>
+    <script src="js/jquery.animateNumber.js "></script>
+    <script src="js/jquery.countdown.js"></script>
+    <script src="js/plugins.js "></script>
+    <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"></script>
+    <script src="js/mapa.js "></script>
+
+
+    <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+    <script>
+        window.ga = function() {
+            ga.q.push(arguments)
+        };
+        ga.q = [];
+        ga.l = +new Date;
+        ga('create', 'UA-XXXXX-Y', 'auto');
+        ga('set', 'anonymizeIp', true);
+        ga('set', 'transport', 'beacon');
+        ga('send', 'pageview')
+    </script>
+    <script src="https://www.google-analytics.com/analytics.js " async></script>
+</body>
+
+</html>
